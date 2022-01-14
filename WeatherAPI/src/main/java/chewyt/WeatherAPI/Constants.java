d52d7a62d999ec94a6b29bc7c16c61e882400cb5
chewyt <yeetian.chew@gmail.com>
2022-01-14 15:33:24 +0800

    remove quotes

--- WeatherAPI/src/main/java/chewyt/WeatherAPI/Constants.java
+++ WeatherAPI/src/main/java/chewyt/WeatherAPI/Constants.java
@@ -3,6 +3,6 @@ package chewyt.WeatherAPI;
 public class Constants {
     
     public static final String URL_WEATHER = "https://api.openweathermap.org/data/2.5/weather";
-    public static final String ENV_WEATHERAPIKEY = ENV_WEATHERAPIKEY;
+    public static final String ENV_WEATHERAPIKEY = System.getenv("ENV_WEATHERAPIKEY");
 
 }
